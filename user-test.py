13df751d98540db9dbefcb41f7acc893962708a5
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-02-15 12:48:02 +0300

    class credentials test

--- user-test.py
+++ user-test.py
@@ -12,15 +12,15 @@ class TestUser(unittest.TestCase):
 		'''
 		Function for creating a user account before each test
 		'''
-		self.new_user = User('Stephen','Ndele','ndele')
+		self.new_user = User('Jackson','Ikonya','Ikonya')
 
 	def test__init__(self):
 		'''
 		Test to if check the initialization/creation of user instances is properly done
 		'''
-		self.assertEqual(self.new_user.first_name,'Stephen')
-		self.assertEqual(self.new_user.last_name,'Ndele')
-		self.assertEqual(self.new_user.password,'ndele')
+		self.assertEqual(self.new_user.first_name,'Jackson')
+		self.assertEqual(self.new_user.last_name,'Ikonya')
+		self.assertEqual(self.new_user.password,'Ikonya')
 
 	def test_save_user(self):
 		'''
@@ -29,51 +29,51 @@ class TestUser(unittest.TestCase):
 		self.new_user.save_user()
 		self.assertEqual(len(User.users_list),1)
 
-# class TestCredentials(unittest.TestCase):
-# 	'''
-# 	Test class that defines test cases for the credentials class behaviours.
-# 	Args:
-# 	    unittest.TestCase: helps in creating test cases
-# 	'''
-# 	def test_check_user(self):
-# 		'''
-# 		Function to test whether the login in function check_user works as expected
-# 		'''
-# 		self.new_user = User('Stephen','Ndele','ndele')
-# 		self.new_user.save_user()
-# 		user2 = User('mark','luke','john')
-# 		user2.save_user()
+class TestCredentials(unittest.TestCase):
+	'''
+	Test class that defines test cases for the credentials class behaviours.
+	Args:
+	    unittest.TestCase: helps in creating test cases
+	'''
+	def test_check_user(self):
+		'''
+		Function to test whether the login in function check_user works as expected
+		'''
+		self.new_user = User('Jackson','Ikonya','Ikonya')
+		self.new_user.save_user()
+		user2 = User('kamau','antony','john')
+		user2.save_user()
 
-# 		for user in User.users_list:
-# 			if user.first_name == user2.first_name and user.password == user2.password:
-# 				current_user = user.first_name
-# 		return current_user
+		for user in User.users_list:
+			if user.first_name == user2.first_name and user.password == user2.password:
+				current_user = user.first_name
+		return current_user
 
-# 		self.assertEqual(current_user,Credential.check_user(user2.password,user2.first_name))
+		self.assertEqual(current_user,Credential.check_user(user2.password,user2.first_name))
 
-# 	def setUp(self):
-# 		'''
-# 		Function to create an account's credentials before each test
-# 		'''
-# 		self.new_credential = Credential('Stephen','Facebook','steve','steve')
+	def setUp(self):
+		'''
+		Function to create an account's credentials before each test
+		'''
+		self.new_credential = Credential('Jackson','Facebook','Ikonya','Ikonya')
 
-# 	def test__init__(self):
-# 		'''
-# 		Test to if check the initialization/creation of credential instances is properly done
-# 		'''
-# 		self.assertEqual(self.new_credential.user_name,'Stephen')
-# 		self.assertEqual(self.new_credential.site_name,'Facebook')
-# 		self.assertEqual(self.new_credential.account_name,'steve')
-# 		self.assertEqual(self.new_credential.password,'steve')
+	def test__init__(self):
+		'''
+		Test to if check the initialization/creation of credential instances is properly done
+		'''
+		self.assertEqual(self.new_credential.user_name,'Jackson')
+		self.assertEqual(self.new_credential.site_name,'Facebook')
+		self.assertEqual(self.new_credential.account_name,'Ikonya')
+		self.assertEqual(self.new_credential.password,'Ikonya')
 
-# 	def test_save_credentials(self):
-# 		'''
-# 		Test to check if the new credential info is saved into the credentials list
-# 		'''
-# 		self.new_credential.save_credentials()
-# 		twitter = Credential('Stephen','Twitter','steve','steve')
-# 		twitter.save_credentials()
-# 		self.assertEqual(len(Credential.credentials_list),2)
+	def test_save_credentials(self):
+		'''
+		Test to check if the new credential info is saved into the credentials list
+		'''
+		self.new_credential.save_credentials()
+		twitter = Credential('Jackson','Twitter','Ikonya','Ikonya')
+		twitter.save_credentials()
+		self.assertEqual(len(Credential.credentials_list),2)
 
 	
 # 	def tearDown(self):
